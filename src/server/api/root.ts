2c73ba0a302a5c3dc095c9e31533036bffee6eb8
thewisepup <wisepup257@gmail.com>
2025-08-20 15:15:39 -0700

    add stubbed posts router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { postRouter } from "~/server/api/routers/post";
 
 /**
  * This is the primary router for your server.
@@ -6,6 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  post: postRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      // TODO: persist to database
+      return {
+        id: 1,
+        name: input.name,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+    }),
+
+  getLatest: publicProcedure.query(async ({ ctx }) => {
+    // TODO: get from database
+    return {
+      id: 1,
+      name: "Hello World",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+  }),
+});
