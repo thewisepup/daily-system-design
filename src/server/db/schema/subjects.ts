156ed1dfa0a942b09e8e08f6d73cd0b839ebb184
thewisepup <wisepup257@gmail.com>
2025-08-20 16:16:52 -0700

    create subjects schema

---
+++ src/server/db/schema/subjects.ts
@@ -0,0 +1,12 @@
+import { pgTable, index, timestamp, text, integer } from "drizzle-orm/pg-core";
+
+export const subjects = pgTable(
+  "subjects",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: text().notNull().unique(),
+    description: text(),
+    createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+  },
+  (table) => [index("subject_name_idx").on(table.name)],
+);
