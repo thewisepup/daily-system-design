98a8bc73c222efa2753bbb7bb59584be272fc955
thewisepup <wisepup257@gmail.com>
2025-09-28 15:07:54 -0700

    creates cron job to send newsletters

--- src/server/email/constants/bulkEmailConstants.ts
+++ src/server/email/constants/bulkEmailConstants.ts
@@ -1,4 +1,4 @@
 export const DB_FETCH_SIZE = 500;
 export const AWS_SES_RATE_LIMIT = 10; // rate limit is 14, but make it 10 here to be safe
-export const BULK_EMAIL_SIZE = 12;
+export const BULK_EMAIL_SIZE = 10;
 export const BULK_EMAIL_DELAY = 1000; // 1 second delay between batches
